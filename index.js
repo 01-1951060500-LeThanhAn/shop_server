2f527db3de124d68899c5ce417e68b67b050892c
lethanhan <lethanhancr7112001@gmail.com>
2023-05-02 21:00:42 +0700

    index

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
-import db from "./database/index.js";
-import userRoutes from "./routes/userRoutes.js";
-import productRoute from "./routes/productRoute.js";
-import cartRoutes from "./routes/cartRoute.js";
-import orderRoutes from "./routes/orderRoute.js";
-import checkoutRote from "./routes/checkoutRoute.js";
-import commentRoute from "./routes/commentRoute.js";
+import db from "./src/database/index.js";
+import userRoutes from "./src/routes/userRoutes.js";
+import productRoute from "./src/routes/productRoute.js";
+import cartRoutes from "./src/routes/cartRoute.js";
+import checkoutRote from "./src/routes/checkoutRoute.js";
+import commentRoute from "./src/routes/commentRoute.js";
 import cors from "cors";
 const app = express();
-db();
+
 dotenv.config();
+db();
 
 app.use(express.json());
 app.use(cors());
 app.use("/users", userRoutes);
 app.use("/product", productRoute);
 app.use("/cart", cartRoutes);
-app.use("/order", orderRoutes);
 app.use("/checkout", checkoutRote);
 app.use("/comment", commentRoute);
 app.listen(process.env.PORT || 8000, async () => {
